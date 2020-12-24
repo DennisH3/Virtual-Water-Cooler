f9b1b8d0b959e7a9cf0465c78c395eb14ce1f5ed
Dennis H <36997325+DennisH3@users.noreply.github.com>
2020-12-24 09:51:28 -0500

    Generate more rows. Added Matched column

--- synthesize_test_data.py
+++ synthesize_test_data.py
@@ -1,10 +1,9 @@
-#
 # A convenient script for generating synthetic data
 #
 # This will save a random_test_data.csv file in the same folder
 #
 # Requirements: pandas, Faker (available on pip)
-# 
+ 
 import pandas as pd
 import random
 
@@ -14,7 +13,7 @@ fake = Faker()
 fake.seed(0)
 
 # The number of rows to generate
-number_of_obs = 100
+number_of_obs = 200
 
 # The minimum amount of interests to generate
 # Must be more x >= 0
@@ -36,7 +35,8 @@ column_names = [
         "When would you like to chat?",
         "Which field are you in?",
         "Do you want to be matched ONLY WITHIN your field?",
-        "What are your interests? Please list your interests separated by a comma with no spaces. I.e. cooking,drawing,etc."
+        "What are your interests? Please list your interests separated by a comma with no spaces. I.e. cooking,drawing,etc.",
+        "Matched"
         ]
 
 # The use of fake.safe_email() here should minimize the damage if someone were
@@ -61,8 +61,11 @@ field_preferences = [random.choice(["Yes", "No"]) for x in range(number_of_obs)]
 # Generate some fake interests
 interests = [",".join(fake.words(nb=random.randint(interests_min, interests_max))) for x in range(number_of_obs)]
 
+# Randomly choose matched status
+match = [random.choice(["Yes", "No"]) for x in range(number_of_obs)]
+
 # Combine everything into a dataframe
-df = pd.DataFrame([emails, names, languages, times, fields, field_preferences, interests]).T
+df = pd.DataFrame([emails, names, languages, times, fields, field_preferences, interests, match]).T
 
 # Set the column names
 df.columns = column_names
